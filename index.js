f61ef31ec27cc250291c90220033762ea784d7d4
Juliano Seus <JulianoSeusDev@outlook.com>
2021-01-29 11:10:52 -0300

    pagina de leitura de artigos

--- index.js
+++ index.js
@@ -36,8 +36,29 @@ app.use("/", CategoriesController);
 app.use("/", ArticlesController);
 
 app.get("/", (req, res) => {
-    Article.findAll().then((articles) => {
+    Article.findAll({
+        order: [
+            ['id', 'DESC']
+        ]
+    }).then((articles) => {
         res.render("index", {articles: articles});
+    });
+});
+
+app.get("/:slug", (req, res) => {
+    var slug = req.params.slug;
+    Article.findOne({
+        where: {
+            slug: slug
+        }
+    }).then((article) => {
+        if (article != undefined) {
+            res.render("article", {article: article});
+        } else {
+            res.redirect("/");
+        }
+    }).catch( err => {
+        res.redirect("/");
     });
 });
 
