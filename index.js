4dc63279ee39c3b1865ff736d75444918c9cb58a
Juliano Seus <JulianoSeusDev@outlook.com>
2021-02-01 10:02:28 -0300

    autenticacao

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const session = require("express-session");
 const connection = require("./database/database")
 
 const CategoriesController = require("./categories/CategoriesController");
@@ -13,6 +14,11 @@ const User = require("./users/User");
 
 //View engine
 app.set('view engine', 'ejs');
+
+// Sessions
+app.use(session({
+    secret: "qualquercoisa", cookie: {maxAge: 300000}
+}));
 
 //Static
 app.use(express.static('public'));
